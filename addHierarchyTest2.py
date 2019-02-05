34ebe53409d7c4c06139a7974f95658b68b598eb
Catherine Zeng <yczeng@mit.edu>
2019-02-05 12:49:25 -0500

    fixed a bug

--- addHierarchyTest2.py
+++ addHierarchyTest2.py
@@ -39,7 +39,6 @@ def addTreeBank(filePath, posVectors, environmentVectors, lexicalVectors, upVect
 
 		for line in parsedTree:
 			tokenizedLine = list(filter(None, line.replace("\n", "").replace(" )", ")").split(" ")))
-			# print(tokenizedLine)
 
 			for count, item in enumerate(tokenizedLine):
 				depth += item.count("(")
@@ -80,14 +79,15 @@ def addTreeBank(filePath, posVectors, environmentVectors, lexicalVectors, upVect
 
 		count = 0
 		movementResults = {}
+		print(sentenceTuple)
 
 		# stores sequences first
 		for i in range(1, len(sentenceTuple)):
 			firstWord = sentenceTuple[i-1][0]
-			firstWordPos = sentenceTuple[i-1][1]
 			firstSequence = sentenceTuple[i-1][2]
 
 			secondWord = sentenceTuple[i][0]
+			secondWordPos = sentenceTuple[i][1]
 			secondSequence = sentenceTuple[i][2]
 
 			count = firstSequence[-1] - secondSequence[0]
@@ -121,17 +121,18 @@ def addTreeBank(filePath, posVectors, environmentVectors, lexicalVectors, upVect
 
 			if firstWord in nonUniqueWords:
 				firstWord = nonUniqueWords[firstWord]
-				print(firstWord)
 			if secondWord in nonUniqueWords:
 				secondWord = nonUniqueWords[secondWord]
 
-			lexicalVectors[firstWord] += environmentVectors[secondWord] * posVectors[firstWordPos] * movementVector
+			print("Saved lexicalVectors", firstWord, ":", secondWord)
+			lexicalVectors[firstWord] += environmentVectors[secondWord] * posVectors[secondWordPos] * movementVector
 
 		for i in range(len(sentenceTuple)):
 			for j in range(len(sentenceTuple)):
 
 				firstWord = sentenceTuple[i][0]
 				secondWord = sentenceTuple[j][0]
+				secondWordPos = sentenceTuple[j][1]
 
 				# check that it's not the same word
 				if firstWord == secondWord or (firstWord + " " + secondWord) in movementResults:
@@ -148,46 +149,114 @@ def addTreeBank(filePath, posVectors, environmentVectors, lexicalVectors, upVect
 
 					movementResults[firstWord + " " + secondWord] = newMovement
 
+					movementVector = None
+					upDownCount = 0
+					# saves into highly dimensional vectors
+					for eachDirection in newMovement:	
+						if eachDirection == "up":
+							if movementVector == None:
+								movementVector = upVector
+								upDownCount += 1
+							else:
+								movementVector *= np.concatenate([upVector[upDownCount:], upVector[:upDownCount]])
+								upDownCount += 1
+						elif eachDirection == "down":
+							if movementVector == None:
+								movementVector = downVector
+								upDownCount += 1
+							else:
+								movementVector *= np.concatenate([downVector[upDownCount:], downVector[:upDownCount]])
+								upDownCount += 1
+
+					firstWordOG = firstWord
+					secondWordOG = secondWord
+					if firstWord in nonUniqueWords:
+						firstWordOG = nonUniqueWords[firstWord]
+					if secondWord in nonUniqueWords:
+						secondWordOG = nonUniqueWords[secondWord]
+
+					lexicalVectors[firstWordOG] += environmentVectors[secondWordOG] * posVectors[secondWordPos] * movementVector
+
 				else:
 					# print(firstWord + " " + secondWord)
 					savedSequence = []
 					
-					pairQueried1 = sentenceTuple[i][0] + " " + sentenceTuple[j-1][0]
-					getSequence1 = movementResults[pairQueried1]
+					pairQueried1 = firstWord + " " + sentenceTuple[j-1][0]
+					pairQueried2 = sentenceTuple[j-1][0] + " " + secondWord
 
-					pairQueried2 = sentenceTuple[j-1][0] + " " + sentenceTuple[j][0]
+					getSequence1 = movementResults[pairQueried1]
 					if pairQueried2 in movementResults:
-						queryResult2 = movementResults[pairQueried2]
+						getSequence2 = movementResults[pairQueried2]
 					else:
 						for i in range(wordFrequency[sentenceTuple[j][0]]):
 							wordWithIndex = sentenceTuple[j][0] + str(i)
 							if sentenceTuple[j-1][0] + " " + wordWithIndex in movementResults:
-								queryResult2 = movementResults[sentenceTuple[j-1][0] + " " + wordWithIndex]
+								getSequence2 = movementResults[sentenceTuple[j-1][0] + " " + wordWithIndex]
 								break
 
-					getSequence2 = queryResult2
-
 					downCount = 0
+					upCount = 0
 					for count, upDown in enumerate(getSequence1):
 						if upDown == "down":
 							downCount += 1
-
-					upCount = 0
 					for count, upDown in enumerate(getSequence2):
 						if upDown == "up":
 							upCount += 1
 
 					minUpDown = min(upCount, downCount)
-					getSequence1 = getSequence1[:-minUpDown]
-					getSequence2 = getSequence2[minUpDown:]
-					savedSequence = getSequence1 + getSequence2
+					savedSequence = getSequence1[:-minUpDown] + getSequence2[minUpDown:]
 					movementResults[firstWord + " " + secondWord] = savedSequence
+
+					movementVector = None
+					upDownCount = 0
+					# saves into highly dimensional vectors
+					for eachDirection in savedSequence:	
+						if eachDirection == "up":
+							if movementVector == None:
+								movementVector = upVector
+								upDownCount += 1
+							else:
+								movementVector *= np.concatenate([upVector[upDownCount:], upVector[:upDownCount]])
+								upDownCount += 1
+						elif eachDirection == "down":
+							if movementVector == None:
+								movementVector = downVector
+								upDownCount += 1
+							else:
+								movementVector *= np.concatenate([downVector[upDownCount:], downVector[:upDownCount]])
+								upDownCount += 1
+
+					firstWordOG = firstWord
+					secondWordOG = secondWord
+					if firstWord in nonUniqueWords:
+						firstWordOG = nonUniqueWords[firstWord]
+					if secondWord in nonUniqueWords:
+						secondWordOG = nonUniqueWords[secondWord]
+
+					lexicalVectors[firstWordOG] += environmentVectors[secondWordOG] * posVectors[secondWordPos] * movementVector
 
 		print("MOVEMENT RESULTS", movementResults)
 		parsedTree = ""
 	
 	text.close()
 	return posVectors, environmentVectors, lexicalVectors
+
+def grabAnalogy(concept1, concept2, analogousTo, lexicalVectors, environmentVectors, numResults=1):
+	f = lexicalVectors[concept1] * lexicalVectors[concept2]
+	analogyResults = {}
+	for word in environmentVectors:
+		if word != analogousTo:
+			analogyResults[word] = np.dot(environmentVectors[word],	environmentVectors[analogousTo] * f)
+
+	print(analogyResults)
+	results = []
+	for i in range(numResults):
+		word = max(analogyResults, key=analogyResults.get)
+
+		results.append((word, analogyResults[word]))
+		analogyResults.pop(word)
+
+	return results
 
 if __name__ == "__main__":
 	posVectors = {}
@@ -196,4 +265,9 @@ if __name__ == "__main__":
 	upVector = np.random.choice([-1, 1], size=10000)
 	downVector = np.random.choice([-1, 1], size=10000)
 
-	posVectors, environmentVectors, lexicalVectors = addTreeBank('data/test2.txt', posVectors, environmentVectors, lexicalVectors, upVector, downVector)
+	# posVectors, environmentVectors, lexicalVectors = addTreeBank('data/test2.txt', posVectors, environmentVectors, lexicalVectors, upVector, downVector)
+
+	posVectors, environmentVectors, lexicalVectors = addTreeBank('data/catDog.txt', posVectors, environmentVectors, lexicalVectors, upVector, downVector)
+	result = grabAnalogy('dog', 'cat', 'purr', lexicalVectors, environmentVectors)
+	print("analogy of cat:purr is dog:", result)
+	print(lexicalVectors["dog"])
